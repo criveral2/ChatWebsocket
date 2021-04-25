6e796d0076f026285810244deeb6b2561a48655c
Christian Rivera <you@example.com>
2021-04-24 20:46:29 -0500

    intercambio de mensajes

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
-//static files
-
-console.log(__dirname)
-//app.use(express.static(''))
-
-
 //settings
 app.set('port', process.env.PORT || 3000);
 
 
+//static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+  
+
+
 //start the server
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
 });
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+//websocket
+io.on('connection' , (socket) =>{
+    console.log('new connection', socket.id);
+
+    socket.on('chat:message', (data) => {
+        console.log(data);
+        io.sockets.emit('chat:message', data);
+    })
+});
+
+
+
 
 
 
